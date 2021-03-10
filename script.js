0e13d1b7fea8ef241ffb1cbac8b87fbf115a9756
Deepika Bekal <deepika.bekal2@gmail.com>
2021-03-10 16:08:03 +0400

    code for password length

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 // Assignment code here
 
+//variable to hold length of the password
+var passwordLength = 0;
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function generatePassword () {
+  //Welcome message
+  alert("Let's get you a new password!");
+  //Password length
+  //prompt to select a length between 8 and 128
+  passLength = prompt("How long do you want your password to be? Choose any length between 8 and 128");
+  //checks if the entry is less than 8 or more than 128. 
+  //If true then sends an alert and asks the user to enter data again
+  //If false then does not enter the loop
+  while (passwordLength < 8 || passwordLength > 128){
+    alert("Not a valid entry. Try again!");
+    passwordLength = prompt("How long do you want your password to be? Choose any length between 8 and 128");
+    
+  };
+  console.log(passwordLength);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generatePassword();
+//Get references to the #generate element
+//var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+//function writePassword() {
+ // var password = generatePassword();
+ // var passwordText = document.querySelector("#password");
+
+  //passwordText.value = password;
+
+//}
+
+//Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword());
